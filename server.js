36b24ac23f01e71a364cd7c811b52720e82e368c
Anay Nayak <anayak007@gmail.com>
2017-09-01 18:18:36 +0530

    Sample webhook response

--- server.js
+++ server.js
@@ -22,6 +22,57 @@ router.use(function (req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
+router.post('/', function(req, res) {
+    console.log(req);
+    var response = 'hello world';
+    var x = {
+        "facebook": {
+            "attachment": {
+                "type": "template",
+                "payload": {
+                    "template_type": "list",
+                    "elements": [
+                        {
+                            "title": "Gehraiyaan Uncut Ep-1",
+                            "image_url": "https://vuclipi-a.akamaihd.net/p/tthumb280x210/v2/d-1/1151929856.jpg",
+                            "default_action": {
+                                "type": "web_url",
+                                "url": "https://web.viu.com/in-hindi/en/video-gehraiyaan_uncut_ep_1-1151929856"
+                            }
+                        },
+                        {
+                            "title": "Gehraiyaan Uncut Ep-2",
+                            "image_url": "https://vuclipi-a.akamaihd.net/p/tthumb280x210/v2/d-1/1152042075.jpg",
+                            "default_action": {
+                                "type": "web_url",
+                                "url": "https://web.viu.com/in-hindi/en/video-gehraiyaan_uncut_ep_2-1152042075"
+                            }
+                        },
+                        {
+                            "title": "Gehraiyaan Uncut Ep-3",
+                            "image_url": "https://vuclipi-a.akamaihd.net/p/tthumb280x210/v3/d-1/1152206104.jpg",
+                            "default_action": {
+                                "type": "web_url",
+                                "url": "https://web.viu.com/in-hindi/en/video-gehraiyaan_uncut_ep_2-1152042075"
+                            }
+                        },
+                        {
+                            "title": "Gehraiyaan Uncut Ep-4",
+                            "image_url": "https://vuclipi-a.akamaihd.net/p/tthumb280x210/v5/d-1/1154884372.jpg",
+                            "default_action": {
+                                "type": "web_url",
+                                "url": "https://web.viu.com/in-hindi/en/video-gehraiyaan_uncut_ep_3-1152206104"
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+    };
+    //res.json(x);
+    res.json( {"speech": response, "displayText": response, data: x});
+});
+
 router.get('/', function (req, res) {
     res.json({
         message: 'hooray! welcome to our api!'
